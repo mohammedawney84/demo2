4581b71a312c78278cd412256d10aadbc6ce7d09
mohammedawney84 <mohammedawney84@gmail.com>
2025-04-18 14:56:17 +0200

    adding remove function to dictionary

--- Dictionary_practice.py
+++ Dictionary_practice.py
@@ -3,3 +3,11 @@ dic1 = {"names":["Ali", "Ahmed"], "Ages":[10, 12]}
 def dic_print(dic):
     for key, value in dic.items():
         print(key, value)
+
+def dic_update(dic):
+    return dic.update(dic)
+
+def dic_delete(dic):
+    return dic.remove(dic)
+
+
