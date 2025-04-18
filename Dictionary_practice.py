5a7dd1ebed21023523b1980e0ae3a347b35db70f
mohammedawney84 <mohammedawney84@gmail.com>
2025-04-18 13:57:58 +0200

    adding new file Dictionary practice

---
+++ Dictionary_practice.py
@@ -0,0 +1,5 @@
+dic1 = {"names":["Ali", "Ahmed"], "Ages":[10, 12]}
+
+def dic_print(dic):
+    for key, value in dic.items():
+        print(key, value)
--- string_practice.py
+++ string_practice.py
@@ -3,5 +3,9 @@ str1 = "my name is Ali"
 def str_capitalize(str):
     return str.capitalize()
 
+def str_upper(str):
+    return str.upper()
+
+
 nw = str_capitalize(str1)
 print(nw)
